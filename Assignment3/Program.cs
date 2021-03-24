4ef49eb61a1e1da50de51b098502e0d5ce439f02
Eden Rose Wallace <wallac33@NSUOK.EDU>
2021-03-23 20:54:38 -0500

    Add project files.

---
+++ Assignment3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+struct Customer
+{
+    public int customerID;
+    public int tellerNo;
+    public int arrivalTime;
+    public int serviceTime;
+    public double balance;
+    public double transactionAmount;
+    public char transactionType;
+    public string firstName;
+    public string lastName; 
+}
